bb0df1bba70157575e6f6ca5aecc766ce56a1a62
Aaron <aaronlhma@gmail.com>
2020-12-09 12:34:32 -0800

    adds submit onclick, blurs img and clears input field

--- scripts/javascripts.js
+++ scripts/javascripts.js
@@ -1,17 +1,27 @@
 // loops through main images and nests them under thumb-bar
 function create_thumbBar(){
     let x = document.getElementsByClassName('thumb');
-    let y = document.getElementById('thumb-bar')
+    let y = document.getElementById('thumb-bar');
     for (i=0;i<5;i++){
-        console.log(x[i].src)
-        y.appendChild(x[i])
+        y.appendChild(x[i]);
     }
 }
 
 function update_image(pic){
-    let x = document.getElementById('displayed-img')
+    let x = document.getElementById('displayed-img');
     if (pic.src != undefined){
     x.src=pic.src;
+    }
+}
+
+function show_buttons(){
+    document.getElementById('form').classList.add('show');
+}
+
+function change_effect(){
+    let x = document.getElementById('effect');
+    if(x.value =='blur'){
+        console.log('input was blur');
     }
 }
 
@@ -21,7 +31,11 @@ function main(){
     ThumbHover.addEventListener('mouseover',function(){
         let target =event.target||event.srcElement;
         update_image(target);
+        show_buttons();
     })
+
+    const Submit = document.getElementsByTagName('button');
+    Submit[0].addEventListener('click',change_effect);
 }
 
 main();
